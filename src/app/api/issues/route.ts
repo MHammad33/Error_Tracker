fbc6e538deeb6aa6faeb3529c9b8c99f5202f2d9
Muhammad Hammad Afzal <mhammadafzal3333@gmail.com>
2025-08-11 16:24:14 +0500

    Build an api for creating the issue

---
+++ src/app/api/issues/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import z from "zod";
+
+const createIssueSchema = z.object({
+	title: z.string().min(1).max(255),
+	description: z.string().min(1),
+});
+
+export async function POST(req: Request) {
+	const body = await req.json();
+	const validation = createIssueSchema.safeParse(body);
+
+	if (!validation.success) {
+		return NextResponse.json(validation.error, { status: 400 });
+	}
+
+	try {
+		const newIssue = await prisma.issue.create({
+			data: {
+				title: body.title,
+				description: body.description,
+			},
+		});
+
+		return NextResponse.json(newIssue, { status: 201 });
+	} catch (error) {
+		if (error instanceof Error) {
+			return NextResponse.json({ message: error.message }, { status: 500 });
+		}
+
+		// Fallback for unknown error shapes
+		return NextResponse.json(
+			{ message: "Internal server error" },
+			{ status: 500 }
+		);
+	}
+}
