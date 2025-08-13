e53c35901336731e63f8e6fc7168d1434ba37ac3
Muhammad Hammad Afzal <mhammadafzal3333@gmail.com>
2025-08-13 12:18:53 +0500

    Add dialog box for delete button

--- src/app/issues/[id]/DeleteIssueButton.tsx
+++ src/app/issues/[id]/DeleteIssueButton.tsx
@@ -1,5 +1,4 @@
-import { Button } from "@radix-ui/themes";
-import Link from "next/link";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import type { FC } from "react";
 import { RiDeleteBin2Fill } from "react-icons/ri";
 
@@ -9,10 +8,33 @@ interface DeleteIssueButtonProps {
 
 const DeleteIssueButton: FC<DeleteIssueButtonProps> = ({ issueId }) => {
 	return (
-		<Button color="red">
-			<RiDeleteBin2Fill />
-			<Link href={`/issues/${issueId}`}>Delete Issue</Link>
-		</Button>
+		<AlertDialog.Root>
+			<AlertDialog.Trigger>
+				<Button color="red">
+					<RiDeleteBin2Fill /> Delete Issue
+				</Button>
+			</AlertDialog.Trigger>
+			<AlertDialog.Content maxWidth="450px">
+				<AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+				<AlertDialog.Description size="2">
+					Are you sure you want to delete this issue? This action cannot be
+					undone.
+				</AlertDialog.Description>
+
+				<Flex gap="3" mt="4" justify="end">
+					<AlertDialog.Cancel>
+						<Button variant="soft" color="gray">
+							Cancel
+						</Button>
+					</AlertDialog.Cancel>
+					<AlertDialog.Action>
+						<Button variant="solid" color="red">
+							Delete Issue
+						</Button>
+					</AlertDialog.Action>
+				</Flex>
+			</AlertDialog.Content>
+		</AlertDialog.Root>
 	);
 };
 
