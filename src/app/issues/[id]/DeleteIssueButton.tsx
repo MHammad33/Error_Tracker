e279f8ac03f671d0acd6b89a3510a64b8517b697
Muhammad Hammad Afzal <mhammadafzal3333@gmail.com>
2025-08-13 12:30:19 +0500

    Handle errors

--- src/app/issues/[id]/DeleteIssueButton.tsx
+++ src/app/issues/[id]/DeleteIssueButton.tsx
@@ -3,7 +3,7 @@
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import type { FC } from "react";
+import { useState, type FC } from "react";
 import { RiDeleteBin2Fill } from "react-icons/ri";
 
 interface DeleteIssueButtonProps {
@@ -11,6 +11,7 @@ interface DeleteIssueButtonProps {
 }
 
 const DeleteIssueButton: FC<DeleteIssueButtonProps> = ({ issueId }) => {
+	const [error, setError] = useState<boolean>(false);
 	const router = useRouter();
 
 	const handleDelete = async () => {
@@ -20,37 +21,57 @@ const DeleteIssueButton: FC<DeleteIssueButtonProps> = ({ issueId }) => {
 			router.refresh();
 		} catch (error) {
 			console.error("Failed to delete issue:", error);
+			setError(true);
 		}
 	};
 
 	return (
-		<AlertDialog.Root>
-			<AlertDialog.Trigger>
-				<Button color="red">
-					<RiDeleteBin2Fill /> Delete Issue
-				</Button>
-			</AlertDialog.Trigger>
-			<AlertDialog.Content maxWidth="450px">
-				<AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-				<AlertDialog.Description size="2">
-					Are you sure you want to delete this issue? This action cannot be
-					undone.
-				</AlertDialog.Description>
+		<>
+			<AlertDialog.Root>
+				<AlertDialog.Trigger>
+					<Button color="red">
+						<RiDeleteBin2Fill /> Delete Issue
+					</Button>
+				</AlertDialog.Trigger>
+				<AlertDialog.Content maxWidth="450px">
+					<AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+					<AlertDialog.Description size="2">
+						Are you sure you want to delete this issue? This action cannot be
+						undone.
+					</AlertDialog.Description>
 
-				<Flex gap="3" mt="4" justify="end">
-					<AlertDialog.Cancel>
-						<Button variant="soft" color="gray">
-							Cancel
-						</Button>
-					</AlertDialog.Cancel>
-					<AlertDialog.Action>
-						<Button variant="solid" color="red" onClick={handleDelete}>
-							Delete Issue
-						</Button>
-					</AlertDialog.Action>
-				</Flex>
-			</AlertDialog.Content>
-		</AlertDialog.Root>
+					<Flex gap="3" mt="4" justify="end">
+						<AlertDialog.Cancel>
+							<Button variant="soft" color="gray">
+								Cancel
+							</Button>
+						</AlertDialog.Cancel>
+						<AlertDialog.Action>
+							<Button variant="solid" color="red" onClick={handleDelete}>
+								Delete Issue
+							</Button>
+						</AlertDialog.Action>
+					</Flex>
+				</AlertDialog.Content>
+			</AlertDialog.Root>
+			<AlertDialog.Root open={error}>
+				<AlertDialog.Content maxWidth="450px">
+					<AlertDialog.Title>Error</AlertDialog.Title>
+					<AlertDialog.Description size="2">
+						An error occurred while trying to delete the issue. Please try again
+						later.
+					</AlertDialog.Description>
+					<Button
+						color="gray"
+						variant="soft"
+						mt="2"
+						onClick={() => setError(false)}
+					>
+						OK
+					</Button>
+				</AlertDialog.Content>
+			</AlertDialog.Root>
+		</>
 	);
 };
 
