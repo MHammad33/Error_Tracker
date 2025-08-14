8243c3b5780ad09db2f9bc3869efd2bef5924748
Muhammad Hammad Afzal <mhammadafzal3333@gmail.com>
2025-08-14 17:54:07 +0500

    populate assignees

--- src/app/issues/[id]/AssigneeSelect.tsx
+++ src/app/issues/[id]/AssigneeSelect.tsx
@@ -1,17 +1,35 @@
+"use client";
+
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import type { FC } from "react";
+import axios from "axios";
+import { useEffect, useState, type FC } from "react";
 
 interface AssigneeSelectProps {}
 
 const AssigneeSelect: FC<AssigneeSelectProps> = ({}) => {
+	const [users, setUsers] = useState<User[]>([]);
+
+	useEffect(() => {
+		const fetchUsers = async () => {
+			const { data } = await axios.get<User[]>("/api/users");
+			setUsers(data);
+		};
+
+		fetchUsers();
+	}, []);
+
 	return (
 		<Select.Root>
 			<Select.Trigger placeholder="Assign..." />
 			<Select.Content>
 				<Select.Group>
 					<Select.Label>Suggestions</Select.Label>
-					<Select.Item value="user1">User 1</Select.Item>
-					<Select.Item value="user2">User 2</Select.Item>
+					{users.map((user) => (
+						<Select.Item key={user.id} value={user.id}>
+							{user.name}
+						</Select.Item>
+					))}
 				</Select.Group>
 			</Select.Content>
 		</Select.Root>
