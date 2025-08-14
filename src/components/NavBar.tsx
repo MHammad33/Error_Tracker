e2c0df322fb1a80c8368972ef6a1fdf280b928f5
Muhammad Hammad Afzal <mhammadafzal3333@gmail.com>
2025-08-14 17:30:07 +0500

    Add loading skeleton in Login

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,6 +13,7 @@ import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
+import Skeleton from "./Skeleton";
 
 const Navbar = ({}) => {
 	return (
@@ -70,7 +71,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
 	const { data: session, status } = useSession();
 
-	if (status === "loading") return null;
+	if (status === "loading") return <Skeleton width="3rem" />;
 
 	if (status === "unauthenticated") {
 		console.log("User is not authenticated");
