3031047586d97def6e271cbdfadf804b7da49572
Muhammad Hammad Afzal <mhammadafzal3333@gmail.com>
2025-08-14 17:26:45 +0500

    refactor navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,8 +15,33 @@ import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 
 const Navbar = ({}) => {
+	return (
+		<nav className="border-b mb-5 px-5 h-14 py-3">
+			<Container>
+				<Flex justify="between">
+					<Flex align="center" gap="3">
+						<Logo />
+						<NavLinks />
+					</Flex>
+					<Box>
+						<AuthStatus />
+					</Box>
+				</Flex>
+			</Container>
+		</nav>
+	);
+};
+
+const Logo = () => {
+	return (
+		<Link href="/">
+			<AiFillBug />
+		</Link>
+	);
+};
+
+const NavLinks = () => {
 	const currentPath = usePathname();
-	const { data: session, status } = useSession();
 
 	const links = [
 		{ href: "/", label: "Dashboard" },
@@ -24,60 +49,59 @@ const Navbar = ({}) => {
 	];
 
 	return (
-		<nav className="border-b mb-5 px-5 h-14 py-3">
-			<Container>
-				<Flex justify="between">
-					<Flex align="center" gap="3">
-						<Link href="/">
-							<AiFillBug />
-						</Link>
-						<ul className="flex space-x-6">
-							{links.map((link) => (
-								<li key={link.href}>
-									<Link
-										href={link.href}
-										className={classNames({
-											"text-zinc-900 font-semibold": currentPath === link.href,
-											"text-zinc-500": currentPath !== link.href,
-											"hover:text-zinc-800 transition-colors": true,
-										})}
-									>
-										{link.label}
-									</Link>
-								</li>
-							))}
-						</ul>
-					</Flex>
-					<Box>
-						{status === "authenticated" && (
-							<DropdownMenu.Root>
-								<DropdownMenu.Trigger>
-									<Avatar
-										src={session.user?.image!}
-										fallback="?"
-										size="2"
-										radius="full"
-										referrerPolicy="no-referrer"
-										className="cursor-pointer"
-									/>
-								</DropdownMenu.Trigger>
-								<DropdownMenu.Content align="end">
-									<DropdownMenu.Label>
-										<Text size="3">{session.user?.email}</Text>
-									</DropdownMenu.Label>
-									<DropdownMenu.Item>
-										<Link href="/api/auth/signout">Log Out</Link>
-									</DropdownMenu.Item>
-								</DropdownMenu.Content>
-							</DropdownMenu.Root>
-						)}
-						{status === "unauthenticated" && (
-							<Link href="/api/auth/signin">Log In</Link>
-						)}
-					</Box>
-				</Flex>
-			</Container>
-		</nav>
+		<ul className="flex space-x-6">
+			{links.map((link) => (
+				<li key={link.href}>
+					<Link
+						href={link.href}
+						className={classNames({
+							"nav-link": true,
+							"!text-zinc-900 font-semibold": currentPath === link.href,
+						})}
+					>
+						{link.label}
+					</Link>
+				</li>
+			))}
+		</ul>
+	);
+};
+
+const AuthStatus = () => {
+	const { data: session, status } = useSession();
+
+	if (status === "loading") return null;
+
+	if (status === "unauthenticated") {
+		console.log("User is not authenticated");
+		return (
+			<Link href="/api/auth/signin" className="nav-link">
+				Log In
+			</Link>
+		);
+	}
+
+	return (
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger>
+				<Avatar
+					src={session!.user!.image!}
+					fallback="?"
+					size="2"
+					radius="full"
+					referrerPolicy="no-referrer"
+					className="cursor-pointer"
+				/>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content align="end">
+				<DropdownMenu.Label>
+					<Text size="3">{session!.user!.email}</Text>
+				</DropdownMenu.Label>
+				<DropdownMenu.Item>
+					<Link href="/api/auth/signout">Log Out</Link>
+				</DropdownMenu.Item>
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
 	);
 };
 
