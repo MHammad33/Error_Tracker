916be70cd4d1e87a2333ed5d54a9ca3af58511a5
Muhammad Hammad Afzal <mhammadafzal3333@gmail.com>
2025-08-14 19:45:25 +0500

    add toast

--- src/app/issues/[id]/AssigneeSelect.tsx
+++ src/app/issues/[id]/AssigneeSelect.tsx
@@ -6,6 +6,7 @@ import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { FC } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 interface AssigneeSelectProps {
 	issueId: string;
@@ -32,27 +33,34 @@ const AssigneeSelect: FC<AssigneeSelectProps> = ({
 	if (isLoading) return <Skeleton height="2rem" />;
 
 	return (
-		<Select.Root
-			defaultValue={assignedUserId || "unassigned"}
-			onValueChange={(userId) => {
-				axios.patch(`/api/issues/${issueId}`, {
-					assignedUserId: userId !== "unassigned" ? userId : null,
-				});
-			}}
-		>
-			<Select.Trigger placeholder="Assign..." />
-			<Select.Content>
-				<Select.Group>
-					<Select.Label>Suggestions</Select.Label>
-					<Select.Item value="unassigned">Unassigned</Select.Item>
-					{users?.map((user) => (
-						<Select.Item key={user.id} value={user.id}>
-							{user.name}
-						</Select.Item>
-					))}
-				</Select.Group>
-			</Select.Content>
-		</Select.Root>
+		<>
+			<Select.Root
+				defaultValue={assignedUserId || "unassigned"}
+				onValueChange={async (userId) => {
+					try {
+						await axios.patch(`/sapi/issues/${issueId}`, {
+							assignedUserId: userId !== "unassigned" ? userId : null,
+						});
+					} catch (error) {
+						toast.error("The assignee could not be updated");
+					}
+				}}
+			>
+				<Select.Trigger placeholder="Assign..." />
+				<Select.Content>
+					<Select.Group>
+						<Select.Label>Suggestions</Select.Label>
+						<Select.Item value="unassigned">Unassigned</Select.Item>
+						{users?.map((user) => (
+							<Select.Item key={user.id} value={user.id}>
+								{user.name}
+							</Select.Item>
+						))}
+					</Select.Group>
+				</Select.Content>
+			</Select.Root>
+			<Toaster />
+		</>
 	);
 };
 
